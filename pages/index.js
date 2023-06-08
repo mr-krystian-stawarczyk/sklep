c3d671b779ff4fd1368adcc1ba8d134483cf2a5f
Krystian S <mr.krystian.stawarczyk@gmail.com>
2023-06-08 21:21:18 +0200

    seted db working

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+import styles from "@/styles/Home.module.scss";
 
 export default function Home() {
 	return (
@@ -14,7 +12,7 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<h1 className={styles.red}>Siema</h1>
+			<button> Siema </button>
 		</div>
 	);
 }
